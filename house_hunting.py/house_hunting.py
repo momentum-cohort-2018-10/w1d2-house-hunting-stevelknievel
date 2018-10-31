1371757f469f46f8bacc2193aff2ea829f332d98
stevelknievel <marks.steven@gmail.com>
2018-10-30 21:30:30 -0400

    removing unnecessary folder.  will re-commit just the new PY file

--- house_hunting.py/house_hunting.py
+++
@@ -1,4 +0,0 @@
-salary = float(input("Starting annual salary? "))
-salary_saved = float(input("Percentage of salary saved? Enter as decimal. "))
-total_cost = float(input("Total cost of dream home? "))
-print("Your annual salary is", salary, "Percentage of salary saved is", salary_saved, "The total cost of your dream home is", total_cost)
